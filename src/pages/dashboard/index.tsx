5b6d409c368e37a6e5839f67b754ba1cb5391a58
Alex Nascimento <alexjose68@hotmail.com>
2024-07-03 16:36:39 -0300

    feat: passei a funcionalidade de deixar a rota da página de /dashboard privada, para que ela só possa ser acessada quando tiver um usuário logado, para isso usei o getSession do next-auth/react que faz verificações e traz dados do lado do servidorantes de montar a tela para o usuário, então passei a lógica para verificar se há usuário logado, e se não tiver usuário logado redireciono para página Home ao tentar entrar na rota de /dashboard, porém, se houver usuário logado, é permitido que acesse normalmente a rota de /dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,5 +1,8 @@
+import { GetServerSideProps } from 'next';
 import styles from './styles.module.css';
 import Head from 'next/head';
+
+import { getSession } from 'next-auth/react';
 
 export default function Dashboard() {
   return (
@@ -11,3 +14,24 @@ export default function Dashboard() {
     </div>
   )
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const session = await getSession( {req} );
+  // console.log(session);
+  // console.log("BUSCANDO PELO SERVER SIDE");
+
+  if (!session?.user) {
+    // Se não tem usuário vamos redirecionar para /
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  }
+
+};
