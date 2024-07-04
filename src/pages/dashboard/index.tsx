4e4c63ab666aa33327ceece7917cbe769c3a30e0
Alex Nascimento <alexjose68@hotmail.com>
2024-07-04 10:38:37 -0300

    feat: instalação da biblioteca react-icons e criação da estrutura e estilização da página de /dashboard, uma área onde ficação o componente Textarea que criamos, um input do tipo checkbox para sinalizar se uma tarefa é pública, e um botão para registar, e uma outra área onde ficarão da tarefas que já foram cadastradas como tags, com ícone para compartilhar se for o caso desta tarefa ser pública, e um ícone para excluir tarefa

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,8 +1,11 @@
 import { GetServerSideProps } from 'next';
 import styles from './styles.module.css';
 import Head from 'next/head';
+import { FaTrash } from 'react-icons/fa';
+import { FiShare, FiShare2 } from 'react-icons/fi';
 
 import { getSession } from 'next-auth/react';
+import { Textarea } from '../../components/textarea';
 
 export default function Dashboard() {
   return (
@@ -10,7 +13,50 @@ export default function Dashboard() {
       <Head>
         <title>Meu painel de Taredas</title>
       </Head>
-      <h1>Página Painel</h1>
+      
+      <main className={styles.main}>
+        <section className={styles.content}>
+          <div className={styles.contentForm}>
+            <h1 className={styles.title}>Qual sua tarefa?</h1>
+
+            <form>
+              <Textarea
+                placeholder='Digite qual sua tarefa...'
+              />
+              <div className={styles.checkboxArea}>
+                <input type="checkbox" className={styles.checkbox} />
+                <label>Deixa tarefa pública</label>
+              </div>
+
+              <button className={styles.button} type='submit'>
+                Registrar
+              </button>
+            </form>
+          </div>
+        </section>
+
+        <section className={styles.taskContainer}>
+          <h1>Minhas tarefas</h1>
+
+          <article className={styles.task}>
+            <div className={styles.tagContainer}>
+              <label className={styles.tag}>PUBLICO</label>
+              <button className={styles.shareButton}>
+                <FiShare2 size={22} color='#3183ff'/>
+              </button>
+            </div>
+
+            <div className={styles.taskContent}>
+              <p>Minha primeira tarefa de exemplo show demais!</p>
+              <button className={styles.trashButton}>
+                <FaTrash size={24} color='#ea3140' />
+              </button>
+            </div>
+          </article>
+
+        </section>
+      </main>
+
     </div>
   )
 }
