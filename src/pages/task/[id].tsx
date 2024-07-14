d2bd4d62985aa8be10f6479d27142a342bd8f2b3
Alex Nascimento <alexjose68@hotmail.com>
2024-07-14 16:28:57 -0300

    feat: Criei e estilizei a página de tarefa, e com o getServerProps usando como parÂmetro o id da tarefa eu busquei os dados da tarefa configurando também se a tarefa não existir ou não for pública o usuário será redirecionado para a página de Home, evitando que ele acesse uma tarefa que não existe ou uma tarefa que não é pública, peguei os dados e passei para um objeto chamado task que tem as propriedades padrão de cada documento da coleção do meu bancode dados, passei este objeto 'task' como propriedade item e a tipei com a interface TaskProps, que aponta que item é um objeto com objetos com suas determinadas propriedades. Então montei e estilizai um article onde a tarefa será renderizada e um Textarea onde o usuário poderá deixar um comentário e enviar ao clicar em um botão!

---
+++ src/pages/task/[id].tsx
@@ -0,0 +1,99 @@
+import { GetServerSideProps } from "next";
+import Head from "next/head";
+import styles from './styles.module.css'
+
+import { db } from '../../services/firebaseConnection';
+import {
+  doc,
+  getDoc,
+  collection,
+  query,
+  where
+} from 'firebase/firestore';
+
+import { Textarea } from '../../components/textarea';
+
+interface TaskProps {
+  item: {
+    tarefa: string;
+    created: string;
+    public: boolean;
+    user: string;
+    taskId: string;
+  }
+}
+
+export default function Task({ item }: TaskProps) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Detalhes da tarefa</title>
+      </Head>
+
+      <main className={styles.main}>
+        <h1>Tarefa</h1>
+        <article className={styles.task}>
+          <p>{item.tarefa}</p>
+        </article>
+      </main>
+
+      <section className={styles.commentsContainer}>
+        <h2>Deixar comentário</h2>
+
+        <form>
+          <Textarea
+            placeholder="Digite o seu comentário..."
+          />
+
+          <button className={styles.button}>Enviar comentário</button>
+        </form>
+      </section>
+    </div>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => {
+  const id = params?.id as string;
+
+  console.log(id);
+
+  const docRef = doc(db, "tarefas", id)
+
+  const snapshot = await getDoc(docRef)
+
+  if (snapshot.data() === undefined) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  };
+
+  if (!snapshot.data()?.public) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      }
+    }
+  }
+
+  const miliseconds = snapshot.data()?.created?.seconds * 1000;
+
+  const task = {
+    tarefa: snapshot.data()?.tarefa,
+    public: snapshot.data()?.public,
+    created: new Date(miliseconds).toLocaleDateString(),
+    user: snapshot.data()?.user,
+    taskId: id,
+  };
+
+  console.log(task);
+
+  return {
+    props: {
+      item: task,
+    },
+  };
+};
