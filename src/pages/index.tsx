1af3d9af6a29e273d353022d3c8e81636b6ce766
Alex Nascimento <alexjose68@hotmail.com>
2024-07-23 17:05:32 -0300

    Feat: criei uma página estática com GetStaticProps, e busquei informação do nosso banco de dados do firebase, para buscar das collections comments e tarefas quantos items/documents tempos em cada uma delas e passar para as caixas da nossa página inicial a quantidade de posts/tarefas e comments/comentários. Para isso também precisei cirar um interface HomeProps com as propriedades posts e comments que criamos no return do nossp getStaticProps, e assim já podemos passar para a nossa interface este retorno.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,23 @@
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import styles from "../../styles/home.module.css";
 import Image from 'next/image'
 
 import heroImg from "../../public/assets/hero.png";
 
+import { db } from '../services/firebaseConnection';
 
-export default function Home() {
+import {
+  collection,
+  getDocs
+} from 'firebase/firestore';
+
+interface HomeProps {
+  posts: number;
+  comments: number;
+}
+
+export default function Home({ posts, comments }: HomeProps) {
   return (
     <div className={styles.container}>
       <Head>
@@ -25,13 +37,31 @@ export default function Home() {
 
         <div className={styles.infoContent}>
           <section className={styles.box}>
-            <span>+12 posts</span>
+            <span>+{posts} posts</span>
           </section>
           <section className={styles.box}>
-            <span>+12 posts</span>
+            <span>+{comments} Comments</span>
           </section>
         </div>
       </main>
     </div>
   );
+}
+
+
+export const getStaticProps: GetStaticProps = async () => {
+  
+  const commentRef = collection(db, "comments");
+  const postRef = collection(db, "tarefas");
+
+  const commentSnapshot = await getDocs(commentRef);
+  const postSnapshot = await getDocs(postRef);
+
+  return {
+    props: {
+      posts: postSnapshot.size || 0,
+      comments: commentSnapshot.size || 0,
+    },
+    revalidate: 60 // Será revalidada a cada 60 segundos...
+  }
 }
